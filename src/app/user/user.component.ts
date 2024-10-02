8ddec5c96ab5a23c846e46f819a62d55c8262bff
Nuraiym Nurbulatova <140681537+nooraiym@users.noreply.github.com>
2024-10-02 19:14:37 +0500

    feat: using signals and input func from core

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,11 +8,19 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-  @Input({required: true}) avatar!: string
-  @Input({required: true}) name!: string
-  get imagePath () {
-    return 'assets/users/' + this.avatar
-  }
+  // @Input({required: true}) avatar!: string
+  // @Input({required: true}) name!: string
+
+  // avatar = input<string>('initialValue') // или так
+  avatar = input.required<string>() // или так
+  name = input.required<string>()
+  imagePath = computed(() => 'assets/users/' + this.avatar())
+
+  // когда мы используем такой синтаксис то предотвращаем изменение значения внутри компонента и нельзя поменять его используя set
+
+  // get imagePath () {
+  //   return 'assets/users/' + this.avatar()
+  // }
 
   onSelectUser () {
   }
