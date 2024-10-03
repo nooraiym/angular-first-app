d030c601d026523f011cafc5488de4ba2e5e0202
nooraiym <140681537+nooraiym@users.noreply.github.com>
2024-10-03 12:10:29 +0500

    refactor: extract types

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,23 +1,28 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+};
+
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrl: './user.component.scss',
 })
 export class UserComponent {
-  @Input({required: true}) id!: string
-  @Input({required: true}) avatar!: string
-  @Input({required: true}) name!: string
-  @Output() select = new EventEmitter<string>()
+  @Input({ required: true }) user!: User;
 
-  get imagePath () {
-    return 'assets/users/' + this.avatar
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
   }
 
-  onSelectUser () {
-    this.select.emit(this.id)
+  onSelectUser() {
+    this.select.emit(this.user.id);
   }
 }
